8e9db49fab7f542c03aa7701cf15ddf53d2f37b8
jrffruit518-enrich <jrffruit518@gmail.com>
2026-02-06 20:21:01 +0100

    Initial commit: clean Monorepo structure with backend and frontend

---
+++ backend/src/main/java/com/rongproject/JavaSprint5_2LibrarySystem/JavaSprint52LibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.rongproject.JavaSprint5_2LibrarySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSprint52LibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSprint52LibrarySystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rongproject/JavaSprint5_2LibrarySystem/JavaSprint52LibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rongproject.JavaSprint5_2LibrarySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSprint52LibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
