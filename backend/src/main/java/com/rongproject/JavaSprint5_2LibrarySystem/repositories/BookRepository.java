bfe9273e43b0049397e8ed864c19510729d8b8e3
jrffruit518-enrich <jrffruit518@gmail.com>
2026-02-09 14:49:39 +0100

    fix : add method

--- backend/src/main/java/com/rongproject/JavaSprint5_2LibrarySystem/repositories/BookRepository.java
+++ backend/src/main/java/com/rongproject/JavaSprint5_2LibrarySystem/repositories/BookRepository.java
@@ -1,10 +1,16 @@
 package com.rongproject.JavaSprint5_2LibrarySystem.repositories;
 
 import com.rongproject.JavaSprint5_2LibrarySystem.entities.Book;
+import com.rongproject.JavaSprint5_2LibrarySystem.exceptions.ResourceNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
     Optional<Book> findByTitle(String title);
+
+    default Book findByIdOrThrow(Long id) {
+        return findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found with ID: " + id));
+    }
 }
